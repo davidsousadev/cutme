baf26aff6a322a148eea0e271f5c12c47e4b7f74
davidplay7 <davidk1k3k@gmail.com>
2024-10-16 19:09:23 -0300

    Ajuste de projeto

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const axios = require('axios');
 const cors = require('cors'); 
-require('dotenv').config();
-const path = require('path');
+const axios = require('axios');
 
+const path = require('path');
 const app = express();
 const port = 3000;
 
 const apiUrl = process.env.URL;
 const apiToken = process.env.RESTDB_TOKEN;
+const dominio = process.env.DOMINIO;
+
+require('dotenv').config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-const dominio = "https://cutme.vercel.app/";
 
 app.use(cors({
   origin: apiUrl, 
@@ -26,6 +26,47 @@ const headers = {
     'x-apikey': apiToken,
     'Cache-Control': 'no-cache'
 };
+
+function generateRandomString(length) {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+}
+
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get(apiUrl, { headers });
+        res.sendFile(path.join(__dirname, 'public', 'index.html')); 
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/lista', async (req, res) => {
+    try {
+        const response = await axios.get(apiUrl, { headers });
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.post('/', async (req, res) => {
+  try {
+    const { url } = req.body; 
+    const randomString = generateRandomString(10); 
+    const urlcut = randomString; 
+    const newBody = { url, urlcut, views: 0 };
+    await axios.post(apiUrl, newBody, { headers });
+    const newUrl = dominio + urlcut;
+    res.status(201).json({ newUrl });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 app.get('/:urlcut', async (req, res) => {
     const { urlcut } = req.params;  
@@ -46,37 +87,6 @@ app.get('/:urlcut', async (req, res) => {
     }
 });
 
-app.post('/', async (req, res) => {
-  try {
-    const { url } = req.body; 
-    const randomString = generateRandomString(10); 
-    const urlcut = randomString; 
-    const newBody = { url, urlcut, views: 0 };
-    await axios.post(apiUrl, newBody, { headers });
-    const newUrl = dominio + urlcut;
-    res.status(201).json({ newUrl });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-app.get('/', async (req, res) => {
-    try {
-        const response = await axios.get(apiUrl, { headers });
-        res.sendFile(path.join(__dirname, 'public', 'index.html')); 
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-app.get('/lista', async (req, res) => {
-    try {
-        const response = await axios.get(apiUrl, { headers });
-        res.json(response.data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
 
 app.get('/:id', async (req, res) => {
     const { id } = req.params;
@@ -87,15 +97,6 @@ app.get('/:id', async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 });
-
-function generateRandomString(length) {
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
-    for (let i = 0; i < length; i++) {
-        result += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    return result;
-}
 
 app.put('/:id', async (req, res) => {
     const { id } = req.params;
