57968d4f766385e9b954d39c84d6308435df33db
davidplay7 <davidk1k3k@gmail.com>
2024-10-27 01:19:38 -0300

    Ajuste de importação notify v2

--- public/js/script.js
+++ public/js/script.js
@@ -1,50 +1,45 @@
 const form = document.getElementById('urlForm');
-        const resultSection = document.getElementById('resultSection');
-        const shortUrlElement = document.getElementById('shortUrl');
-        const copyButton = document.getElementById('copyButton');
+const resultSection = document.getElementById('resultSection');
+const shortUrlElement = document.getElementById('shortUrl');
+const copyButton = document.getElementById('copyButton');
 
-        form.addEventListener('submit', async (event) => {
-            event.preventDefault();
-            const url = document.getElementById('urlInput').value;
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const url = document.getElementById('urlInput').value;
 
-            try {
-                const response = await fetch('/', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ url })
-                });
-
-                const data = await response.json();
-
-                if (response.ok) {
-                    shortUrlElement.innerText = data.newUrl; 
-                    resultSection.style.display = 'block';
-                    copyButton.style.display = 'block';
-                } else {
-                    mostrarNotificacaotexto('Erro ao encurtar a URL.');
-                }
-            } catch (error) {
-                console.error('Erro:', error);
-                mostrarNotificacaotexto('Erro ao encurtar a URL.');
-            }
+    try {
+        const response = await fetch('/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ url })
         });
 
-        function copyToClipboard() {
-            const urlText = shortUrlElement.innerText;
+        const data = await response.json();
 
-            navigator.clipboard.writeText(urlText)
-                .then(() => {
-                    mostrarNotificacaotexto('URL copiada para a área de transferência!');
-                })
-                .catch(err => {
-                    console.error('Erro ao copiar URL: ', err);
-                });
+        if (response.ok) {
+            shortUrlElement.innerText = data.newUrl; 
+            resultSection.style.display = 'block';
+            copyButton.style.display = 'block';
+            mostrarNotificacao('Sua URL foi encurtada com sucesso!');
+        } else {
+            mostrarNotificacao('Erro ao encurtar a URL.');
         }
+    } catch (error) {
+        console.error('Erro:', error);
+        mostrarNotificacao('Erro ao encurtar a URL.');
+    }
+});
 
-        function mostrarNotificacaotexto(texto) {
-            const notificacao = criarNotificacao(texto);
-            adicionarNotificacaoAoContainer(notificacao);
-            gerenciarNotificacoes();
-        }
+function copyToClipboard() {
+    const urlText = shortUrlElement.innerText;
+
+    navigator.clipboard.writeText(urlText)
+        .then(() => {
+            mostrarNotificacao('URL copiada para a área de transferência!');
+        })
+        .catch(err => {
+            console.error('Erro ao copiar URL: ', err);
+        });
+}
